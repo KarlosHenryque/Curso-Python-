c85ecd6f0af16f1427499a98d28a0123d90d191a
KarlosHenryque <karloshenryquep@gmail.com>
2025-07-22 19:03:29 -0300

    Aula 04

---
+++ Logica/Aula04/Main.py
@@ -0,0 +1,19 @@
+# Tipos int e float
+
+#int 
+print('Int')
+print(11)
+print(-11)
+print(0)
+
+#float 
+print('\nFloat')
+print(1.1)
+print(-1.1)
+print(0.0)
+
+#Função para saber o tipo do dado
+print('\nType')
+print( type('Karlos') )
+print( type(1) )
+print( type(1.0) )
